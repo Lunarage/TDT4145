16ed9f22ba2dd65e49db982ea2d5adee1303a6dd
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-02-22 17:44:09 +0100

    Fix syntax error #1
    - Alle `REFERENCES` måtte legges inn som en egen `CONSTRAINT`.
    - Nøkkelordet `CHECK` må legges inn som en `CONSTRAINT`, og kan ikke
      spesifiseres rett i opprettelsen av kolonnen.
    - `ENUM` kan kun inneholde string literals i quotes `'`.
    - `IF NOT EXISTS` må komme før tabellnavnet.

--- mysql/create_tables.sql
+++ mysql/create_tables.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Serier IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Serier 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     tittel VARCHAR(200)
 );
 
-CREATE TABLE Personer IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Personer 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(100),
@@ -12,7 +12,7 @@ CREATE TABLE Personer IF NOT EXISTS
     land VARCHAR(100)
 );
 
-CREATE TABLE Selskaper IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Selskaper 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(200),
@@ -21,18 +21,19 @@ CREATE TABLE Selskaper IF NOT EXISTS
     nettside VARCHAR(100)
 );
 
-CREATE TABLE Titler IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Titler 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     tittel VARCHAR(200),
     lanseringsdato DATE,
-    laget_for ENUM(NULL, 'kino', 'tv', 'streaming', 'video')
-    lengde INT, -- minutter
-    beskrivelse TEXT,
-    utgiver_id INT REFERENCES Selskaper ON UPDATE CASCADE ON DELETE SET NULL
+    laget_for VARCHAR(50),
+    lengde INTEGER, -- minutter
+    beskrivelse VARCHAR(500),
+    utgiver_id INTEGER,
+    CONSTRAINT titler_utgiver_fk FOREIGN KEY (utgiver_id) REFERENCES Selskaper(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
-CREATE TABLE Brukere IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Brukere 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     brukernavn VARCHAR(50),
@@ -41,68 +42,85 @@ CREATE TABLE Brukere IF NOT EXISTS
     land VARCHAR(100)
 );
 
-CREATE TABLE Kategorier IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Kategorier 
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     navn VARCHAR(30),
     beskrivelse TEXT
 );
 
-CREATE TABLE Roller IF NOT EXISTS 
+CREATE TABLE IF NOT EXISTS Roller  
 (
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
+    tittel_id INTEGER,
     skuespiller_id INTEGER REFERENCES Personer ON UPDATE CASCADE ON DELETE CASCADE,
     rollenavn VARCHAR(40),
-    CONSTRAINT roller_pk PRIMARY KEY (tittel_id, skuespiller_id, rollenavn)
+    CONSTRAINT roller_pk PRIMARY KEY (tittel_id, skuespiller_id, rollenavn),
+    CONSTRAINT roller_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT roller_skuespiller_fk FOREIGN KEY (skuespiller_id) REFERENCES Personer(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Anmeldelser IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Anmeldelser 
 (
-    bruker_id INTEGER REFERENCES Brukere ON UPDATE CASCADE ON DELETE CASCADE,
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    rating INTEGER CHECK rating >= 0 AND rating <= 10,
-    tekst TEXT,
-    CONSTRAINT anmeldelser_pk PRIMARY KEY (bruker_id, tittel_id)
+    bruker_id INTEGER,
+    tittel_id INTEGER,
+    rating INTEGER,
+    tekst VARCHAR(1000),
+    CONSTRAINT anmeldelser_pk PRIMARY KEY (bruker_id, tittel_id),
+    CONSTRAINT anmeldelser_bruker_fk FOREIGN KEY (bruker_id) REFERENCES Brukere(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT anmeldelser_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT rating_ok CHECK ((rating) >= 0 AND (rating) <= 10)
 );
 
-CREATE TABLE Episoder IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Episoder 
 (
-    serie_id INTEGER REFERENCES Serier ON UPDATE CASCADE ON DELETE CASCADE,
-    episode_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT episoder_pk PRIMARY KEY (serie_id, episode_id)
+    serie_id INTEGER,
+    episode_id INTEGER,
+    CONSTRAINT episoder_pk PRIMARY KEY (serie_id, episode_id),
+    CONSTRAINT epsioder_serie_fk FOREIGN KEY (serie_id) REFERENCES Serier(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT episoder_episode_fk FOREIGN KEY (episode_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Regissorer IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Regissorer 
 (
-    regissor_id INTEGER REFERENCES Personer ON UPDATE CASCADE ON DELETE CASCADE,
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT regissorer_pk PRIMARY KEY (regissor_id, tittel_id)
+    regissor_id INTEGER,
+    tittel_id INTEGER,
+    CONSTRAINT regissorer_pk PRIMARY KEY (regissor_id, tittel_id),
+    CONSTRAINT regissorer_person_fk FOREIGN KEY (regissor_id) REFERENCES Personer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT regissorer_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Manusforfattere IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Manusforfattere 
 (
-    forfatter_id INTEGER REFERENCES Personer ON UPDATE CASCADE ON DELETE CASCADE,
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT manusforfattere_pk PRIMARY KEY (forfatter_id, tittel_id)
+    forfatter_id INTEGER,
+    tittel_id INTEGER,
+    CONSTRAINT manusforfattere_pk PRIMARY KEY (forfatter_id, tittel_id),
+    CONSTRAINT manusforfattere_personer_fk FOREIGN KEY (forfatter_id) REFERENCES Personer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT manusforfattere_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Musikere IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Musikere 
 (
-    musiker_id INTEGER REFERENCES Personer ON UPDATE CASCADE ON DELETE CASCADE,
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT musikere_pk PRIMARY KEY (musiker_id, tittel_id)
+    musiker_id INTEGER REFERENCES Personer,
+    tittel_id INTEGER REFERENCES Titler,
+    CONSTRAINT musikere_pk PRIMARY KEY (musiker_id, tittel_id),
+    CONSTRAINT musikere_person_fk FOREIGN KEY (musiker_id) REFERENCES Personer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT musikere_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Komponister IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Komponister 
 (
-    komponist_id INTEGER REFERENCES Personer ON UPDATE CASCADE ON DELETE CASCADE,
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT komponister_pk PRIMARY KEY (komponist_id, tittel_id)
+    komponist_id INTEGER REFERENCES Personer,
+    tittel_id INTEGER REFERENCES Titler,
+    CONSTRAINT komponister_pk PRIMARY KEY (komponist_id, tittel_id),
+    CONSTRAINT komponister_person_fk FOREIGN KEY (komponist_id) REFERENCES Personer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT komponister_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE Tittel_i_kategori IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS Tittel_i_kategori 
 (
-    tittel_id INTEGER REFERENCES Titler ON UPDATE CASCADE ON DELETE CASCADE,
-    kategori_id INTEGER REFERENCES Kategorier ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT tittel_i_kategori_pk PRIMARY KEY (tittel_id, kategori_id)
+    tittel_id INTEGER REFERENCES Titler,
+    kategori_id INTEGER REFERENCES Kategorier,
+    CONSTRAINT tittel_i_kategori_pk PRIMARY KEY (tittel_id, kategori_id),
+    CONSTRAINT kategori_tittel_fk FOREIGN KEY (tittel_id) REFERENCES Titler(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT kategori_kategori_fk FOREIGN KEY (kategori_id) REFERENCES Kategorier(id) ON UPDATE CASCADE ON DELETE CASCADE
 )
