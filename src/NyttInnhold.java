a4fa392cfc434d8755814f2486cb2b6a4e84f644
Magne Halvorsen <maghalv@stud.ntnu.no>
2020-03-06 14:55:22 +0100

    Refactored and implemented insert class

--- src/NyttInnhold.java
+++ src/NyttInnhold.java
@@ -3,6 +3,9 @@
  * @author Magne Halvorsen
 */
 
+import java.util.List;
+//More on lists:
+//https://www.javatpoint.com/java-arraylist
 import java.sql.*;
 //More on JDBC
 //https://www.javatpoint.com/java-jdbc
@@ -11,44 +14,77 @@ public class NyttInnhold extends DBConn{
 
 private String query;
 
-public void nyttSelskap(){
-  query = "INSERT INTO Selskaper VALUES ()";
-}
-
-public void nyPerson(){
-
-}
-
-public void nyRolle(){
-
-}
-
-public void nySerie(){
-
-}
-
-public void nyKategori(){
-
-}
-
-public void nyRegissor(){
-
-}
-
-public void nyForfatter(){
-
-}
-
-public void nyMusiker(){
-
-}
-
-public void nyKomponist(){
-
-}
-
-public void nyTittel(){
-
+public void settInn(String tabell, List<String> verdier) throws SQLException{
+    switch(tabell){
+        case "Selskap":
+            query = "INSERT INTO Selskaper "
+                  + "(navn, adresse, land, nettside) "
+                  + "VALUES (?, ?, ?, ?)";
+        break;
+        case "Person":
+            query = "INSERT INTO Personer "
+                  + "(navn, bursdag, land) "
+                  + "VALUES (?, ?, ?)";
+        break;
+        case "Rolle":
+            query = "INSERT INTO Roller "
+                  + "(tittel_id, skuespiller_id, rollenavn) "
+                  + "VALUES (?, ?, ?)";
+        break;
+        case "Serie":
+            query = "INSERT INTO Serier "
+                  + "(tittel) "
+                  + "VALUES (?)";
+        break;
+        case "Kategori":
+            query = "INSERT INTO Kategorier "
+                  + "(navn, beskrivelse) "
+                  + "VALUES (?, ?)";
+        break;
+        case "Regissor":
+            query = "INSERT INTO Regissorer "
+                  + "(regissor_id, tittel_id) "
+                  + "VALUES (?, ?)";
+        break;
+        case "Forfatter":
+            query = "INSERT INTO Manusforfattere "
+                  + "(forfatter_id, tittel_id) "
+                  + "VALUES (?, ?)";
+        break;
+        case "Musiker":
+            query = "INSERT INTO Musikere "
+                  + "(musiker_id, tittel_id) "
+                  + "VALUES (?, ?)";
+        break;
+        case "Komponist":
+            query = "INSERT INTO Komponister "
+                  + "(komponist_id, tittel_id) "
+                  + "VALUES (?, ?)";
+        break;
+        case "Tittel":
+            query = "INSERT INTO Titler "
+                  + "(tittel, lanseringsdato, laget_for,"
+                  + " lengde, beskrivelse, utgiver_id) "
+                  + "VALUES (?, ?, ?, ?, ?, ?)";
+        break;
+        default:
+            query = "";
+    }
+    if(!query.isEmpty()){
+        PreparedStatement statement = conn.prepareStatement(query);
+        ParameterMetaData meta = statement.getParameterMetaData();
+        if(meta.getParameterCount() == verdier.size()){
+            for(Integer i = 0; i < verdier.size(); i++){
+                statement.setString(i+1, verdier.get(i));
+            }
+            statement.executeUpdate();
+        }else{
+            System.out.println("Mismatch antall parametere");
+        }
+        statement.close();
+    }else{
+        System.out.println("Ugyldig tabell.");
+    }
 }
 
 }
