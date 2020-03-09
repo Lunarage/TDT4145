c96cca8c6fe19b5495e07921cbe4121be5559b49
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2020-03-09 15:52:27 +0100

    Add a bunch of episodes

--- mysql/filmer.sql
+++ mysql/filmer.sql
@@ -42,7 +42,9 @@ INSERT INTO Selskaper (id, navn, adresse, land, nettside)
 VALUES
 (1, 'Warner Bros.','3400 Warner Blvd, Burbank, CA 91505','USA','www.warnerbros.com/'),
 (2, 'Paramount Pictures','5555 Melrose Avenue, Hollywood, California','USA','www.paramount.com/'),
-(3, 'Miramax','Los Angeles, California','USA','www.miramax.com')
+(3, 'Miramax','Los Angeles, California','USA','www.miramax.com'),
+(4, 'Home Box Office', 'New York City, New York', 'USA', 'www.hbo.com'),
+(5, 'Netflix', 'Los Gatos, California', 'USA', 'www.netflix.com')
 ;
 
 INSERT INTO Titler (id, tittel, lanseringsdato, laget_for, lengde, beskrivelse, utgiver_id)
@@ -61,8 +63,24 @@ VALUES
     3),
 (5, 'No Country for Old Men','2007-11-21','Kino',122,
     'In rural Texas, welder and hunter Llewelyn Moss (Josh Brolin) discovers the remains of several drug runners who have all killed each other in an exchange gone violently wrong. Rather than report the discovery to the police, Moss decides to simply take the two million dollars present for himself. This puts the psychopathic killer, Anton Chigurh (Javier Bardem), on his trail as he dispassionately murders nearly every rival, bystander and even employer in his pursuit of his quarry and the money. As Moss desperately attempts to keep one step ahead, the blood from this hunt begins to flow behind him with relentlessly growing intensity as Chigurh closes in. Meanwhile, the laconic Sheriff Ed Tom Bell (Tommy Lee Jones) blithely oversees the investigation even as he struggles to face the sheer enormity of the crimes he is attempting to thwart.',
-    3)
-;
+    3),
+    (6, 'Winter is Coming', '2011-05-04', 'TV', 62, 'Eddard Stark is torn between his family and an old friend when asked to serve at the side of King Robert Baratheon; Viserys plans to wed his sister to a nomadic warlord in exchange for an army.', 4),
+    (7, 'The Kingsroad', '2011-05-11', 'TV', 56, 'While Bran recovers from his fall, Ned takes only his daughters to King''s Landing. Jon Snow goes with his uncle Benjen to the Wall. Tyrion joins them.', 4),
+    (8, 'Lord Snow', '2011-05-01', 'TV', 58, 'Jon begins his training with the Night''s Watch; Ned confronts his past and future at King''s Landing; Daenerys finds herself at odds with Viserys.', 4),
+    (9, 'Cripples, Bastards, and Broken Things', '2011-05-08', 'TV', 56, 'Eddard investigates Jon Arryn''s murder. Jon befriends Samwell Tarly, a coward who has come to join the Night''s Watch.', 4),
+    (10, 'The Wolf and the Lion', '2011-05-15', 'TV', 55, 'Catelyn has captured Tyrion and plans to bring him to her sister, Lysa Arryn, at the Vale, to be tried for his, supposed, crimes against Bran. Robert plans to have Daenerys killed, but Eddard refuses to be a part of it and quits.', 4),
+    (11, 'A Golden Crown', '2011-05-22', 'TV', 53, 'While recovering from his battle with Jaime, Eddard is forced to run the kingdom while Robert goes hunting. Tyrion demands a trial by combat for his freedom. Viserys is losing his patience with Drogo.', 4),
+    (12, 'You Win or You Die', '2011-05-29', 'TV', 58, 'Robert has been injured while hunting and is dying. Jon and the others finally take their vows to the Night''s Watch. A man, sent by Robert, is captured for trying to poison Daenerys. Furious, Drogo vows to attack the Seven Kingdoms.', 4),
+    (13, 'The Pointy End', '2011-06-05', 'TV', 59, 'The Lannisters press their advantage over the Starks; Robb rallies his father''s northern allies and heads south to war; The White Walkers attack the Wall; Tyrion returns to his father with some new friends.', 4),
+    (14, 'Baelor', '2011-06-12', 'TV', 57, 'Robb goes to war against the Lannisters. Jon finds himself struggling on deciding if his place is with Robb or the Night''s Watch. Drogo has fallen ill from a fresh battle wound. Daenerys is desperate to save him.', 4),
+    (15, 'Fire and Blood', '2011-06-19', 'TV', 53, 'Robb vows to get revenge on the Lannisters. Jon must officially decide if his place is with Robb or the Night''s Watch. Daenerys says her final goodbye to Drogo.', 4),
+    (16, 'Currahee', '2001-09-09', 'TV', 70, 'Easy Company goes through training under the leadership of a captain who relentlessly pushes them to their limits but may be limited as a leader in the field.', 4),
+    (17, 'Day of Days', '2001-09-09', 'TV', 64, 'Easy Company paratroopers jump behind enemy lines in Normandy on D-Day and struggle to reunite in hostile territory.', 4),
+    (18, 'Carentan', '2001-09-16', 'TV', 62, 'Easy Company takes on German troops in the French town of Carentan, and the battle takes its toll on one soldier who is badly traumatized by the experience.', 4),
+    (19, 'Replacements', '2001-09-23', 'TV', 57, 'With the addition of many new men, Easy Company heads to Holland to participate in Operation Market Garden and prepare an Allied route into Germany, but they meet stiff German resistance.', 4),
+    (20, 'Crossroads', '2001-09-30', 'TV', 53, 'Winters writes a report on the challenge of an unexpected resistance to a German attack, and is haunted by his conscience after shooting a teenage German SS soldier.', 4),
+    (21, 'Bastogne', '2001-10-07', 'TV', 64, 'In the dead of winter, the men of Easy Company fend off frostbite and hunger - in addition to the enemy - while holding the line in a forest outside of Bastogne. Meanwhile, exhausted medic Eugene Roe finds friendship with a Belgian nurse.', 4),
+    (22, 'All to Play For', '2019-03-08', 'TV', 40, 'Driver Daniel Ricciardo looks to make a statement on the track while the teams prepare for the first race of the season at the Australian Grand Prix.', 5);
 
 INSERT INTO Roller (tittel_id, skuespiller_id, rollenavn)
 VALUES
@@ -129,3 +147,31 @@ VALUES
 (5,7),
 (5,4)
 ;
+
+INSERT INTO Serier
+    (id, tittel)
+VALUES
+    (1, 'Game of Thrones'),
+    (2, 'Band of Brothers'),
+    (3, 'Formula 1: Drive to Survive');
+
+INSERT INTO Episoder
+    (serie_id, episode_id, sesong, episodenummer)
+VALUES
+    (1, 6, 1, 1),
+    (1, 7, 1, 2),
+    (1, 8, 1, 3),
+    (1, 9, 1, 4),
+    (1, 10, 1, 5),
+    (1, 11, 1, 6),
+    (1, 12, 1, 7),
+    (1, 13, 1, 8),
+    (1, 14, 1, 9),
+    (1, 15, 1, 10),
+    (2, 16, 1, 1),
+    (2, 17, 1, 2),
+    (2, 18, 1, 3),
+    (2, 19, 1, 4),
+    (2, 20, 1, 5),
+    (2, 21, 1, 6),
+    (3, 22, 1, 1);
